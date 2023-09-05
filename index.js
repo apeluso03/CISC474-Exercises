d9733f302e3d7113bdd8e7025f6acebce21cf139
apeluso03 <124719953+apeluso03@users.noreply.github.com>
2023-09-05 08:02:12 -0400

    Got name to display properly using jQuery

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
     console.log("ready!");
-});
 
+    $("#fname").click(function(){
+        $(this).val("");
+    });
 
-$("#fname").change(function(){
-    $(this).val("");
-});
+    $("#lname").click(function(){
+        $(this).val("");
+    });
 
-$("#lname").change(function(){
-    $(this).val("");
-});
+    $("#go").click(function(){
+        console.log("Handler for 'click' called");
+        var value = $("#fname").val() + " " + $("#lname").val();
+        $("#jumbotron").text(value);
+        $("#fname").val();
+        $("#lname").val();
+        return false;
+    });
 
-$("#go").on("click", function(){
-    console.log("Handler for 'click' called");
-    var value = $("#fname").val() + $("#lname").val();
-    $("#jumbotron").text(value);
-    return false;
 });
